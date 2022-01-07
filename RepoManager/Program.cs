6bc39fb2c657a036c8f1364969e19f8130d193bc
Steve Teece <sgteece@hotmail.com>
2022-01-07 17:17:44 +1100

    Add Nuget Packages and Controls
    Added Nuget packages needed for SyncFusion components and Azure KeyVault Secrets.
    
    Created controls for the Organisation Tree and Repository grid. Populated both controls with sample data from SF docs.
    
    Deleted unrequired files from the Blazor WASM template

--- RepoManager/Program.cs
+++ RepoManager/Program.cs
@@ -1,11 +1,32 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RepoManager;
+using Syncfusion.Blazor;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+//TODO: Put code to access azure Keyvault here 
+
+// Declare constants for KeyVault
+//const string AZUREAPPID = "ebf89c68-57a8-4142-b3d4-f542f14730bd";
+//const string SYNCFUSIONSECRET = "e577Q~UQ4~bbpKqZxSc6StDHdUvyKE3sJGWzG";
+const string KEYVAULTURI = "https://bulkrepokeyvault.vault.azure.net/";
+
+//SecretClient secretClient = new SecretClient(new Uri(KEYVAULTURI), new DefaultAzureCredential());
+//KeyVaultSecret sfkey = secretClient.GetSecret("SyncFusionLicenseKey");
+
+//var sfLicenseKey = sfkey.Value.ToString();
+
+// Register Syncfusion License
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register SyncFusion Blazor Service
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
