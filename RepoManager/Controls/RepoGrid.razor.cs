41bbabf412836922cba6971e8c884c4d9cc4555b
Steve Teece <sgteece@hotmail.com>
2022-01-08 11:11:21 +1100

    Update Sample Data Grid
    Update the Repo Grid configuration.  Change sample data to reflect predicted actual data

---
+++ RepoManager/Controls/RepoGrid.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using RepoManager.Models;
+
+namespace RepoManager.Controls
+{
+    public partial class RepoGrid : ComponentBase
+    {
+        public List<RepoListItemModel> Repos { get; set; }
+
+        public RepoGrid(List<RepoListItemModel> repos)
+        {
+            Repos = repos;
+        }
+
+        public RepoGrid()
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            Repos = Enumerable.Range(1, 23).Select(x => new RepoListItemModel($"Test Data Repository {x}")).ToList();
+        }
+
+    }
+}
---
+++ RepoManager/Models/RepoListItemModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RepoManager.Models
+{
+    public class RepoListItemModel
+    {
+        public bool IsSelected { get; set; } = false;
+        public string RepoName { get; set; }
+
+        public RepoListItemModel(string name)
+        {
+            RepoName = name;
+        }
+        public RepoListItemModel()
+        {
+
+        }
+    }
+}
