8a1ccc43efbfb9023b7e382f36739fbfad6264e7
Steve Teece <sgteece@hotmail.com>
2022-01-12 16:17:33 +1100

    Remove old unrequried code and comments

--- RepoManager/Program.cs
+++ RepoManager/Program.cs
@@ -2,23 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RepoManager;
 using Syncfusion.Blazor;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 
-//TODO: Put code to access azure Keyvault here 
-
-// Declare constants for KeyVault
-//const string AZUREAPPID = "ebf89c68-57a8-4142-b3d4-f542f14730bd";
-//const string SYNCFUSIONSECRET = "e577Q~UQ4~bbpKqZxSc6StDHdUvyKE3sJGWzG";
-const string KEYVAULTURI = "https://bulkrepokeyvault.vault.azure.net/";
-
-//SecretClient secretClient = new SecretClient(new Uri(KEYVAULTURI), new DefaultAzureCredential());
-//KeyVaultSecret sfkey = secretClient.GetSecret("SyncFusionLicenseKey");
-
-//var sfLicenseKey = sfkey.Value.ToString();
-
-// Register Syncfusion License
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTYzNDUyQDMxMzkyZTM0MmUzMEVDemJmQzZHVGFSb2MxbmthdXQ4eHNCVjB1a2twZThoUENRcGlBTENnTnM9");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
